8099ee86d2d1f0297a1a7cda5cf32f48bb48b741
Vandewaeter Fabio <vandewaeter.fabio@gmail.com>
2024-12-21 17:51:12 +0100

    trying to fix Mine

--- include/structures/activeStructures/Mine.hpp
+++ include/structures/activeStructures/Mine.hpp
@@ -4,16 +4,21 @@
 #include "ActiveStructure.hpp"
 
 class Faction;
+class Tile;
+class Item;
 
 class Mine : public ActiveStructure
 {
 public:
-    Mine(Texture *texture, CollisionManager *collisionManager, EntityManager *entityManager, SDL_Rect hitBox, unsigned int HP, Faction *faction);
+    Mine(Texture *texture, CollisionManager *collisionManager, EntityManager *entityManager, SDL_Rect hitBox, unsigned int HP, Faction *faction, Tile *minedTile);
     ~Mine();
 
     void update();
 
 private:
+    Item *minedResource;
+    int capacity;
+    int quantityResource;
 };
 
 #endif
--- src/structures/activeStructures/Mine.cpp
+++ src/structures/activeStructures/Mine.cpp
@@ -1,12 +1,17 @@
 #include "structures/activeStructures/Mine.hpp"
 
-Mine::Mine(Texture *texture, CollisionManager *collisionManager, EntityManager *entityManager, SDL_Rect hitBox, unsigned int HP, Faction *faction) : ActiveStructure(texture, collisionManager, entityManager, hitBox, HP, true, faction) {}
+Mine::Mine(Texture *texture, CollisionManager *collisionManager, EntityManager *entityManager, SDL_Rect hitBox, unsigned int HP, Faction *faction, Tile *minedTile) : ActiveStructure(texture, collisionManager, entityManager, hitBox, HP, true, faction)
+{
+    this->minedResource = minedTile->getResource();
+    this->capacity = 50;
+    this->quantityResource = 0;
+}
 Mine::~Mine() {}
 
 void Mine::update()
 {
-    if (this->active)
+    if (this->active && (this->quantityResource < this->capacity))
     {
-
+        quantityResource++;
     }
 }
