775c3cb6e47865999316bb420f4f80e63a45d9af
Vandewaeter Fabio <vandewaeter.fabio@gmail.com>
2025-01-10 20:27:50 +0100

    save

--- src/renderer.c
+++ src/renderer.c
@@ -4,7 +4,6 @@
 #include <assert.h>
 #include <stdio.h>
 #include "renderer.h"
-#include "atlas.inl"
 
 static SDL_Window *window;
 static SDL_Renderer *renderer;
@@ -62,18 +61,18 @@ void r_draw_text(const char *text, mu_Vec2 pos, mu_Color color) {
 
 
 void r_draw_icon(int id, mu_Rect rect, mu_Color color) {
-  mu_Rect src = atlas[id];
+  /*mu_Rect src = atlas[id];
   int x = rect.x + (rect.w - src.w) / 2;
-  int y = rect.y + (rect.h - src.h) / 2;
+  int y = rect.y + (rect.h - src.h) / 2;*/
 }
 
 int r_get_text_width(const char *text, int len) {
   int res = 0;
-  for (const char *p = text; *p && len--; p++) {
+  /*for (const char *p = text; *p && len--; p++) {
     if ((*p & 0xc0) == 0x80) { continue; }
     int chr = mu_min((unsigned char) *p, 127);
     res += atlas[ATLAS_FONT + chr].w;
-  }
+  }*/
   return res;
 }
 
